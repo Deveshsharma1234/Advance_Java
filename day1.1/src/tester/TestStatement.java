c4ae59c59089b1c9d70cfa25386ce37082ef619c
Devesh Sharma <deveshsharma372002@gmail.com>
2025-11-07 22:40:43 +0530

    Fetching data from db

--- day1.1/src/tester/TestStatement.java
+++ day1.1/src/tester/TestStatement.java
@@ -2,6 +2,8 @@ package tester;
 import static util.DBUtils.openConnection;
 import java.sql.*;
 
+import model.EmpModel;
+
 
 
 public class TestStatement {
@@ -12,9 +14,17 @@ public class TestStatement {
 				ResultSet rst = st.executeQuery("select * from my_emp");
 				)
 		{
+			System.out.println("Exection of query started");
 			while(rst.next()) {
-				
-				
+//				System.out.println(rst.toString());
+				  EmpModel emp = new EmpModel();
+				    emp.setEmp_id(rst.getInt("emp_id"));
+				    emp.setName(rst.getString("emp_name"));
+				    emp.setJobTitle(rst.getString("job_title"));
+				    emp.setDept(rst.getString("department"));
+				    emp.setSal(rst.getBigDecimal("salary"));
+				    emp.setHireDate(rst.getDate("hire_date"));
+				    System.out.println(emp.toString());				
 			}
 					
 			
